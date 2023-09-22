032cf695ea8f739436f849cecc74d9ae672162d2
SanSaich <kurdenkov93@gmail.com>
2023-09-22 17:25:43 +0400

    style: page case

--- src/pages/cases/PageCase.vue
+++ src/pages/cases/PageCase.vue
@@ -1,18 +1,23 @@
 <template>
-    <h5>{{ $route.params.id }}</h5>
+    <q-page class="relative-position">
+        <q-scroll-area class="q-px-md absolute full-width full-height">
+            <h5 class="q-my-md text-weight-medium">
+                {{ post.title }}
+            </h5>
+            <p>{{ post.text }}</p>
 
-    <template v-if="post">
-        <q-img
-            :src="
-                post.file && post.file.url
-                    ? post.file.url
-                    : 'https://cdn.quasar.dev/img/mountains.jpg'
-            "
-            spinner-color="primary"
-        />
-        <p>{{ post.title }}</p>
-        <p>{{ post.text }}</p>
-    </template>
+            <template v-if="post">
+                <q-img
+                    :src="
+                        post.file && post.file.url
+                            ? post.file.url
+                            : 'https://cdn.quasar.dev/img/mountains.jpg'
+                    "
+                    spinner-color="primary"
+                />
+            </template>
+        </q-scroll-area>
+    </q-page>
 </template>
 
 <script>
